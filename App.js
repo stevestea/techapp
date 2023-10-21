aca6f979e35e4b8410fa81774e291241012872a0
unknown <baogia23112006@gmail.com>
2023-10-21 20:51:10 +0700

    upload code

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.tieude}>xinchao</Text>
+      <Button></Button>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    
+  },
+  tieude: {
+    fontSize: 30 ,
+    color: 'red'
+  }
+});
